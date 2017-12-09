daba004d7e1ccc82caa17a98dbcd788de45b59d4
Shane Staret <shanestaret@gmail.com>
2017-12-09 17:48:48 -0500

    Add files via upload

---
+++ Week 6/JAVA/Tabs.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by Shane on 12/1/2017.
+ */
+public class Tabs extends JFrame {
+    public Tabs() {
+
+        setTitle("Tabbed Pane"); //title of the figure
+        JTabbedPane jtp = new JTabbedPane(); //creates the area the tabs will reside within
+        getContentPane().add(jtp); //adds the area created above to an arraylist of areas (size 1 because we only want one section of tabs)
+        ImageIcon oImg = new ImageIcon("C:\\Users\\Shane\\Desktop\\MATLAB\\imageLogo.png"); //creates the logo that will represent the first tab
+        Image image = oImg.getImage(); //changes the ImageIcon into an Image so we can scale it to an appropriate size
+        Image newimg = image.getScaledInstance(50, 40,  java.awt.Image.SCALE_SMOOTH); // scale the logo in the smooth way
+        oImg = new ImageIcon(newimg);  // transform it back into an ImageIcon
+        ImageIcon mImg = new ImageIcon("C:\\Users\\Shane\\Desktop\\MATLAB\\barLogo.png"); //creates the logo that will represent the second tab
+        Image image2 = mImg.getImage(); //changes the ImageIcon into an Image so we can scale it to an appropriate size
+        Image newimg2 = image2.getScaledInstance(50, 40, java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        mImg = new ImageIcon(newimg2);  // transform it back into an ImageIcon
+        JLabel label1 = new JLabel(new ImageIcon("C:\\Users\\Shane\\Desktop\\MATLAB\\MainFigure.png")); //creates the image we actually want to see when clicking the first tab
+        JLabel label2 = new JLabel(new ImageIcon("C:\\Users\\Shane\\Desktop\\MATLAB\\BarFigure.png")); //creates the image we actually want to see when clicking the second tab
+        JPanel jp1 = new JPanel(); //creates a panel where we will put the pictures, color lines, and 3D cluster plot
+        JPanel jp2 = new JPanel(); //creates a panel where we will put the bar graphs
+        jp1.add(label1); //adds the actual image of the pictures, color lines, and 3D cluster plot to the panel
+        jp2.add(label2); //adds the actual image of the bar graphs to the panel
+        jtp.addTab("",oImg, jp1); //actually creates the individual first tab
+        jtp.setMnemonicAt(0, KeyEvent.VK_1); //if the first tab is clicked, then the first panel will show
+        jtp.addTab("",mImg, jp2); //actually creates the individual second tab
+        jtp.setMnemonicAt(1, KeyEvent.VK_2); //if the second tab is clicked, then the second panel will show
+    }
+}
---
+++ Week 6/MATLAB/ColorLine.m
@@ -0,0 +1,54 @@
+classdef ColorLine %class to create color variation line
+    
+    properties
+        ColorProps
+        ColorLineXStart
+        numberOfFramesDisplayed %defining all important properties that need to be defined in constructor specifically
+        ColorValues
+    end
+    
+    methods
+        function lineObj = ColorLine(c,x,l,RGB)
+            lineObj.ColorProps = c; %an array that holds all of the color proportions of the image
+            lineObj.ColorLineXStart = x; %accepts the starting x-value for the colorline (changes with each frame)
+            lineObj.numberOfFramesDisplayed = l; %number of frames that will actually be displayed
+            lineObj.ColorValues = RGB; %RGB values of all the colors we want to appear on the color line
+        end
+        
+        function drawLine = drawLine(lineObj)
+            bound = lineObj.ColorProps(1); %the y-value that we start to draw the rectangle at; equivalent to the upper y-value of last rectangle drawn; so if there are three rectangles on top of one another and the second one ends at y = 15, then bound = 15, and bound will be used to determine the lower y-value for the next rectangle 
+            if(lineObj.numberOfFramesDisplayed == 1) %if we are looking at an image, plot the line at x = 4.5 so it is in the middle of the subplot
+                lineObj.ColorLineXStart = 4.5;
+            end
+            %---------------------------------
+            for i=1:length(lineObj.ColorProps)
+                if i == 1
+            linePortion = rectangle('Position', [lineObj.ColorLineXStart 0 1 bound], 'FaceColor', lineObj.ColorValues(i,:),'LineStyle','none'); %draws the first line, since we are looking at the propOfWhite, we make the line white; we change the width as well to make it more visible
+                else
+            linePortion = rectangle('Position', [lineObj.ColorLineXStart bound 1 lineObj.ColorProps(i)], 'FaceColor', lineObj.ColorValues(i,:),'LineStyle','none'); %draws the first line, since we are looking at the propOfWhite, we make the line white; we change the width as well to make it more visible
+                end
+            if i > 1
+            bound = bound + lineObj.ColorProps(i);
+            end
+            end
+            % Code from here to hyphenated line is to draw the color
+            % variation line. Essentially, the first rectangle must start
+            % at y = 0 and end at whatever proportion of "white" there is
+            % in the image. So if the image is 10% white, then a white
+            % rectangle is drawn from y = 0 to y = 10. Then, then the next
+            % loop will draw a Dark Grey rectangle from y = 10 to whatever
+            % the proportion of Dark Grey is plus ten. So say Dark Grey is
+            % 3% of the image, then that rectangle is drawn from y = 10 to
+            % y = 13. This goes all the way until y = 100, as all of the
+            % individual color proportions add to 100%.
+            ylim([0 100]); %sets it so y is only displayed up until 100; only have this because since our line goes all the way to 100, MatLab automatically scales it so 0 to 120 is shown, which looks worse. This line fixes that display bug.
+            xlim([0 10]); %sets it so x limit by default is 10, but that can change depending on how many frames we are looking at
+            if(lineObj.numberOfFramesDisplayed > 10)
+            xlim([0 lineObj.numberOfFramesDisplayed]); %for formatting, we want the color variation line to take up the entire plot, but not look too thin or wide. So the upper limit for x is based strictly on the number of frames we are looking at. For example, if we are looking at 50 frames, there will be 50 color lines, each of width one. So we want the plot to go from x = 0 to x = 50 so the whole plot is filled. So the first line goes from x = 0 to x = 1, then x = 1 to x = 2, etc.
+            end
+            axis off %turns the axis off so it looks much nicer
+        end %turns off the axis because we do not need to see them unless we are debugging to ensure proportions are correct
+            
+    end
+
+end
