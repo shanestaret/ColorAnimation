5ce5f3835d7ff8b99307f62a6f104e7d8c11a93e
Shane Staret <shanestaret@gmail.com>
2017-12-11 14:28:18 -0500

    Create FileCreator.java

---
+++ Week 6/JAVA/FileCreator.java
@@ -0,0 +1,12 @@
+import java.io.*; //import to properly create and write to the file
+
+// one class needs to have a main() method
+public class FileCreator
+{
+  public static void create(int n) throws IOException {
+        File fileOfMaxColorIndex = new File("C://Users//Shane//Desktop//maxColorIndex.txt"); //creates new text file on desktop
+        FileWriter writer = new FileWriter(fileOfMaxColorIndex); //creates an object that can be used to write to the file we have created
+        writer.write(n); //writes the maxColorIndex to the text file, so if maxColorIndex is "3", the text file will now contain "3"
+        writer.close(); //closes the object used to write to the text file
+  }
+}
