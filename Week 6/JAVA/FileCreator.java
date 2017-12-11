6819ef4bc7355ff90c338c98f71c3b03b0db01bb
Shane Staret <shanestaret@gmail.com>
2017-12-11 14:52:19 -0500

    Update FileCreator.java

--- Week 6/JAVA/FileCreator.java
+++ Week 6/JAVA/FileCreator.java
@@ -13,7 +13,7 @@ public class FileCreator
 			System.out.println("File not created.");
     
         FileWriter writer = new FileWriter(fileOfMaxColorIndex); //creates an object that can be used to write to the file we have created
-        writer.write(Integer.toString(n)); //writes the maxColorIndex to the text file, so if maxColorIndex is "3", the text file will now contain "3"
+        writer.write(Integer.toString(n)); //writes the maxColorIndex to the text file, so if maxColorIndex is "3", the text file will now contain "3" as a String
         writer.close(); //closes the object used to write to the text file
   }
 }
