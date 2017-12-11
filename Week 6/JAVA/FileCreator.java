159cc91c27dc1e2c5ec215a5dd3389cb360616a2
Shane Staret <shanestaret@gmail.com>
2017-12-11 14:58:54 -0500

    Update FileCreator.java

--- Week 6/JAVA/FileCreator.java
+++ Week 6/JAVA/FileCreator.java
@@ -5,12 +5,15 @@ import java.io.*; //import to properly create and write to the file
 public class FileCreator
 {
   public static void create(int n) throws IOException {
-        File fileOfMaxColorIndex = new File("C://Users//Shane//Desktop//maxColorIndex.txt"); //creates new text file on desktop
+	String fileLocation = "C://Users//Shane//Desktop//maxColorIndex.txt" //specify where the file should be saved
+        File fileOfMaxColorIndex = new File(fileLocation); //creates new text file on desktop
     
     	if (fileOfMaxColorIndex.createNewFile()) //prints whether the file was successfully created or not
 			System.out.println("New file successfully created.");
+	  	elseif(fileOfMaxColorIndex.exists())
+			System.out.println("File successfully updated.");
 		else
-			System.out.println("File updated.");
+			System.out.println("There was a problem when creating/updating the file.");
     
         FileWriter writer = new FileWriter(fileOfMaxColorIndex); //creates an object that can be used to write to the file we have created
         writer.write(Integer.toString(n)); //writes the maxColorIndex to the text file, so if maxColorIndex is "3", the text file will now contain "3" as a String
