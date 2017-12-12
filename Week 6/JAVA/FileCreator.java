70331bfc660f59f3ded3757754d4417e0fcd2af3
Shane Staret <shanestaret@gmail.com>
2017-12-11 19:50:18 -0500

    Update FileCreator.java

--- Week 6/JAVA/FileCreator.java
+++ Week 6/JAVA/FileCreator.java
@@ -1,21 +1,21 @@
-package MATLAB;
+package MATLAB;//package MATLAB
 import java.io.*; //import to properly create and write to the file
 
 public class FileCreator
 {
-  public static void create(int n) throws IOException {
-	String fileLocation = "C://Users//Shane//Desktop//maxColorIndex.txt"; //specify where the file should be saved
+    public static void create(int n) throws IOException {
+        String fileLocation = "C://Users//Shane//Desktop//maxColorIndex.txt"; //specify where the file should be saved
         File fileOfMaxColorIndex = new File(fileLocation); //creates new text file on desktop
-    
-    	if (fileOfMaxColorIndex.createNewFile()) //prints whether the file was successfully created or not
-		System.out.println("New file successfully created.");
-	elseif(fileOfMaxColorIndex.exists())
-		System.out.println("File successfully updated.");
+
+        if (fileOfMaxColorIndex.createNewFile()) //prints whether the file was successfully created or not
+            System.out.println("New file successfully created.");
+        else if(fileOfMaxColorIndex.exists())
+        System.out.println("File successfully updated.");
         else
-		System.out.println("There was a problem when creating/updating the file.");
-    
+        System.out.println("There was a problem when creating/updating the file.");
+
         FileWriter writer = new FileWriter(fileOfMaxColorIndex); //creates an object that can be used to write to the file we have created
         writer.write(Integer.toString(n)); //writes the maxColorIndex to the text file, so if maxColorIndex is "3", the text file will now contain "3" as a String
         writer.close(); //closes the object used to write to the text file
-  }
+    }
 }
