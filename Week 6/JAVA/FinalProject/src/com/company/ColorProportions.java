82267b313e98d5b9903f35c1caefa45fe2d4096a
Shane Staret <shanestaret@gmail.com>
2017-12-11 20:39:02 -0500

    Add files via upload

---
+++ Week 6/JAVA/FinalProject/src/com/company/ColorProportions.java
@@ -0,0 +1,63 @@
+package com.company; //package MATLAB
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader; //Imports to read from a file
+import java.io.IOException;
+import java.util.ArrayList; //import to utilize ArrayList
+
+/**
+ * Created by Shane on 12/1/2017.
+ */
+public class ColorProportions {
+    private static String line = null; //String that will represent every individual line of text file
+    private static ArrayList<Double> colors = new ArrayList<>(); //ArrayList to hold all color proportions acquired from text file
+    private static int maxColorIndex; //int that will hold the final index that holds the max proportion
+
+    public static int retrieveMaxColor() throws IOException {
+        File txtFile = new File("C:\\Users\\Shane\\Desktop\\MATLAB\\propsArray.txt"); //grab the text file
+        FileReader reader = new FileReader(txtFile); //read the text file
+        BufferedReader bufferedReader = new BufferedReader(reader); //read the text file in such a way we can loop through the individual lines
+        while ((line = bufferedReader.readLine()) != null) { //while there is another line in the text file to read
+            colors.add(Double.parseDouble(line)); //add that line into the color proportions ArrayList (there are 32 lines to represent the 32 colors)
+        }
+        double propSum = 0; //double that will hold the sum of all color props we must combine; so this sum will hold DarkRed, Red, and LightRed to represent all the Reds
+        int count = 0; //integer to determine when all the color props we must combine are added together
+        int nextIndex = 1; //the index we want to put the new proportion within (goes from 1 to 8 because 0 is White in the old and new array and 9 is black in the old and new array)
+        for (int i = 0; i < colors.size(); i++) { //loops through every element of color props array (there are 32 elements)
+            if ((i > 0 && i < 13) || (i > 18 && i < 22) || (i > 27 && i < 31)) { //this if statement gets the color props combination for colors that only have three to combine; so DarkRed, Red, and LightRed proportions are all combined to make Red here
+                propSum += colors.get(i); //adds up the proportions that must be combined, so if DarkRed = 20, Red = 10, and LightRed = 15, then the new array will have Red = 45
+                count++; //counts up so we know we already did one of the proportions that must be combined
+                if (count == 3) { //once all three are combined
+                    colors.set(nextIndex, propSum); //set the next index in the new array equal to the combined proportions; now that Red = 45, the second element in the array is equal to 45, and the first element is equal to the white proportion
+                    count = 0; //resets this so we can move onto the next proportions that must be combined
+                    propSum = 0; //reset this so that the next proportion combinations can properly be added together
+                    nextIndex++; //increases the index that the combined color proportions are added to so we simply don't replace them; 0 is white, 1 is Red, 2 is Orange, etc.
+                }
+            }
+            if ((i > 12 && i < 19) || (i > 21 && i < 28)) { //this if statement gets the color props combination for colors that have six to combine; so DarkSkyBlue, SkyBlue, LightSkyBlue, DarkBlue, Blue, and LightBlue proportions are all combined to make Blue here
+                propSum += colors.get(i); //adds up the proportions that must be combined, so if DarkSkyBlue = 20, SkyBlue = 10, and LightSkyBlue = 3, DarkBlue = 1, Blue = 2, and LightBlue = 2 then the new array will have Blue = 38
+                count++; //counts up so we know we already did one of the proportions that must be combined
+                if (count == 6) { //once all six are combined
+                    colors.set(nextIndex, propSum); //set the next index in the new array equal to the combined proportions; now that Blue = 38, the sixth element in the array is equal to 38
+                    count = 0; //resets this so we can move onto the next proportions that must be combined
+                    propSum = 0; //reset this so that the next proportion combinations can properly be added together
+                    nextIndex++; //increases the index that the combined color proportions are added to so we simply don't replace them; 0 is white, 1 is Red, 2 is Orange, etc.
+                }
+            }
+            if (i == 31) //if we are looking at the last element of the array (black)
+                colors.set(9, colors.get(i)); //set it to the tenth element of the array, as we want the last element of our array to be 9 since we only have ten colors (0 to 9)
+        }
+        for (int i = 0; i < 22; i++) { //array was initially size 32, but now we only want it to be size 10, so we remove the uneeded elements 22 times since there are 22 of them
+            colors.remove(10); //removes all of the extra elements that are no longer needed, so we only have the ten combined proportions
+        }
+        double max = 0; //double that will hold the largest percentage
+        for (int i = 0; i < colors.size(); i++) { //loops through the 10 new and combined proportions
+            if (colors.get(i) > max) { //if the element we are looking at has a greater proportion than any of the previous elements
+                maxColorIndex = i; //then the index with the greatest proportion is now equal to this element
+                max = colors.get(i); //the new max is equal to this element's proportion
+            }
+        }
+        return maxColorIndex; //returns the max index; so if we have array [1, 5, 3, 1], then it will return "1" because 5 is the largest proportion and it is in the second index (after the zeroth index)
+    }
+}
---
+++ Week 6/JAVA/FinalProject/src/com/company/FileCreator.java
@@ -0,0 +1,21 @@
+package com.company;//package MATLAB
+import java.io.*; //import to properly create and write to the file
+
+public class FileCreator
+{
+    public static void create(int n) throws IOException {
+        String fileLocation = "C://Users//Shane//Desktop//maxColorIndex.txt"; //specify where the file should be saved
+        File fileOfMaxColorIndex = new File(fileLocation); //creates new text file on desktop
+
+        if (fileOfMaxColorIndex.createNewFile()) //prints whether the file was successfully created or not
+            System.out.println("New file successfully created.");
+        else if(fileOfMaxColorIndex.exists())
+        System.out.println("File successfully updated.");
+        else
+        System.out.println("There was a problem when creating/updating the file.");
+
+        FileWriter writer = new FileWriter(fileOfMaxColorIndex); //creates an object that can be used to write to the file we have created
+        writer.write(Integer.toString(n)); //writes the maxColorIndex to the text file, so if maxColorIndex is "3", the text file will now contain "3" as a String
+        writer.close(); //closes the object used to write to the text file
+    }
+}
